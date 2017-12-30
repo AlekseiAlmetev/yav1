ea435d11601929e49c58174c4ba6ec7975d18a7c
lexa.almetiev <lexa.almetiev@yandex.ru>
2017-12-30 11:31:52 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Folders.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerCon
+{
+    class Folders //для папки и для содержимого
+    
+    {
+        private string name;
+
+        private Boolean initialized;
+
+        private List<Finfo> files;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Boolean Initialized
+        {
+            get { return initialized; }
+            set { initialized = value; }
+        }
+
+        public List<Finfo> Files
+        {
+            get { return files; }
+            set { files = value; }
+        }
+    }
+}
